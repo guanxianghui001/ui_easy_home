d47db1d1cb9653bcaad1e8e1f635582d7e6da68c
guanxianghui <guanxianghui@croot.com>
2023-06-29 17:45:28 +0800

    更新启动文件

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from uvicorn import run
 
 app = FastAPI(debug=True)
 
@@ -6,3 +7,8 @@ app = FastAPI(debug=True)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+if __name__ == '__main__':
+    # 运行fastapi程序
+    run(app="main:app", host="127.0.0.1", port=8000, reload=True)

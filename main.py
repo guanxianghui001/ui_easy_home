f48e2ca0aea083aa682d25602c99b7f9cc750798
huxueyan <huxueyan@croot.com>
2023-06-30 18:19:11 +0800

    1

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+import uvicorn
+from routes.module import module_add
 
 app = FastAPI(debug=True)
 
@@ -6,3 +8,13 @@ app = FastAPI(debug=True)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/addmodule")
+async def root():
+    return module_add('hu')
+
+
+
+if __name__ == '__main__':
+    # 运行fastapi程序
+    uvicorn.run(app="main:app", host="127.0.0.1", port=8000, reload=True)
---
+++ routes/module.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+def module_add(name):
+    print(name)
+    return {"name": name}
